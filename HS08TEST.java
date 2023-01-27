b2a24c9b1f0c85298da3cafb21fd17b5bb5833bf
manish26eleven <118613667+manish26eleven@users.noreply.github.com>
2023-01-27 07:46:39 +0530

    Add files via upload

---
+++ HS08TEST.java
@@ -0,0 +1,24 @@
+// We have populated the solutions for the 10 easiest problems for your support.
+// Click on the SUBMIT button to make a submission to this problem.
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String[] args) throws Exception{
+        InputStreamReader i = new InputStreamReader(System.in);
+        BufferedReader bf = new BufferedReader(i);
+        String[] in = bf.readLine().split(" ");
+        float n = Float.parseFloat(in[0]);
+        float f = Float.parseFloat(in[1]);
+        
+        if(n%5==0 && f>=n+0.5f)
+        System.out.println(f-n-0.5f);
+        
+        else{
+            System.out.println(f);
+        }
+        
+    }
+}
+
