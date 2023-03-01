af2b599265cec24ba43cd7ba152e7d45c4f6ba72
manish26eleven <118613667+manish26eleven@users.noreply.github.com>
2023-03-01 20:03:00 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    static int fib(int n) {
+        if(n <=1) {
+            return n;
+        }
+            return (fib(n-1)+fib(n-2));
+        }
+           public static void main(String args[]) {
+               Scanner sc=new Scanner(System.in);
+               int n=sc.nextInt();
+               fib(n);
+           }
+    }
